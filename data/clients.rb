fa1685fbd42c3e6acdc11f39f1b5fc823c9165e3
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-05-11 14:21:16 +0100

    Move data to separate folder

---
+++ data/clients.rb
@@ -0,0 +1,100 @@
+def getClients
+  [
+    {
+      name: 'WikiJob Ltd',
+      logo: 'clients/wikijob.png',
+      url: 'http://wikijob.co.uk',
+      description: "UK's largest graduate careers website",
+      projects: [
+        {
+          name: 'WikiJob',
+          url: 'http://wikijob.co.uk',
+        },
+        {
+          name: 'WikiFestivals',
+          url: 'http://www.wikifestivals.com',
+        },
+        {
+          name: 'Mission Control',
+          url: 'http://missioncontrolhq.com',
+        },
+      ],
+    },
+    {
+      name: 'Planet Illogica, Inc',
+      logo: 'clients/planetillogica.png',
+      url: 'http://www.planetillogica.com',
+      description: 'Self-empowering online artistic community for artists and creative-minded individuals. ',
+      projects: [
+        {
+          name: 'Planet illogica',
+          url: 'http://www.planetillogica.com',
+        },
+      ],
+    },
+    {
+      name: '',
+      logo: 'clients/adyax.png',
+      url: 'http://www.adyax.com/en',
+      description: 'Largest Drupal Shops in Europe with near 100 persons dedicated to Drupal.',
+      projects: [
+#          {
+#            name: 'Les Editions de la Cigale',
+#            url: 'http://www.editions-cigale.com',
+#          },
+      ],
+    },
+    {
+      name: 'National Association for the Education of Young Children',
+      logo: 'clients/naeyc.png',
+      url: 'http://www.naeyc.org',
+      description: "World's largest organization working on behalf of young children.",
+      projects: [
+        {
+          name: 'NAEYC / store',
+          url: 'http://www.naeyc.org',
+        },
+      ],
+    },
+    {
+      name: 'Development Mill',
+      logo: 'clients/developmentmill.png',
+      url: 'http://developmentmill.com',
+      description: 'Russian offshore software development provider.',
+      projects: [
+#         {
+#           name: 'Les Editions de la Cigale',
+#           url: 'http://www.editions-cigale.com',
+#         },
+        {
+          name: 'Planet illogica',
+          url: 'http://www.planetillogica.com',
+        },
+        {
+          name: 'NAEYC / store',
+          url: 'http://www.naeyc.org',
+        },
+        {
+          name: 'Hi-Human Technologies',
+          url: 'http://hi-human.ru',
+        },
+      ],
+    },
+    {
+     name: 'Omsk State Technical University',
+     logo: 'clients/omgtu.png',
+     url: 'http://omgtu.ru',
+     description: 'Russian Scientific-technological university.',
+     projects: [
+       {
+         name: 'Educational & Methodical Center',
+         url: 'http://tso.omgtu.ru',
+       },
+       {
+         name: 'Computer Science and Engineering',
+         url: 'http://ivt.omgtu.ru',
+       },
+     ],
+    },
+  ]
+end
---
+++ data/skills.rb
@@ -0,0 +1,28 @@
+def getSkills
+  [
+    {
+      name: 'Frontend',
+      items: ['HTML', 'HAML', 'CSS', 'SASS','LESS', 'JavaScript', 'CoffeeScript', 'jQuery', 'AJAX', 'Bootstrap'],
+    },
+    {
+      name: 'Backend',
+      items: ['Drupal', 'PHP', 'MySQL', 'SaaS', 'Ruby on Rails', 'Ruby', 'Python'],
+    },
+    {
+      name: 'Testing',
+      items: ['SimpleTest', 'Behat', 'Cucumber'],
+    },
+    {
+      name: 'Mobile',
+      items: ['PhoneGap', 'jQuery Mobile', 'Backbonejs', 'Spinejs', 'Emberjs'],
+    },
+    {
+      name: 'System Administration',
+      items: ['Linux', 'Bash', 'Nginx', 'Apache', 'Varnish', 'Chef', 'Drush', 'High Performance Optimization', 'Rackspace Cloud', 'Amazon Web Services', 'Heroku', 'Pantheon'],
+    },
+    {
+      name: 'Teamwork',
+      items: ['BDD', 'TDD', 'Git', 'Mercurial', 'Subversion'],
+    },
+  ]
+end
