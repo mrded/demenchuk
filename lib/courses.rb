23939fe6a0aa2dc9a1c21da6f436adfa76b951e5
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-06-23 00:09:55 +0100

    Change style

--- lib/courses.rb
+++ lib/courses.rb
@@ -3,6 +3,12 @@ module Demenchuk
     {
       name: 'BerkeleyX',
       content: "CS169.1x Software as a Service",
+      logo: 'https://portland2013.drupal.org/sites/default/files/DCPDX-badge-round-I-go.png',
+      url: 'https://s3.amazonaws.com/verify.edx.org/downloads/62cf2d4f510f48ee95e44f01e6bc7243/Certificate.pdf',
+    },
+    {
+      name: 'BerkeleyX',
+      content: "CS169.1x Software as a Service",
       logo: '/images/courses/cs169.1x.png',
       url: 'https://s3.amazonaws.com/verify.edx.org/downloads/62cf2d4f510f48ee95e44f01e6bc7243/Certificate.pdf',
     },
