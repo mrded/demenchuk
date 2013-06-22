37500e27fc21312729245d396eefdc44d471a843
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-06-22 23:43:23 +0100

    Add Static popovers.

--- lib/courses.rb
+++ lib/courses.rb
@@ -2,19 +2,19 @@ module Demenchuk
   COURSES = [
     {
       name: 'BerkeleyX',
-      description: "CS169.1x Software as a Service",
+      content: "CS169.1x Software as a Service",
       logo: '/images/courses/cs169.1x.png',
       url: 'https://s3.amazonaws.com/verify.edx.org/downloads/62cf2d4f510f48ee95e44f01e6bc7243/Certificate.pdf',
     },
     {
       name: 'BerkeleyX',
+      content: "CS169.2x Software as a Service",
       logo: '/images/courses/cs169.2x.png',
       url: 'https://s3.amazonaws.com/verify.edx.org/downloads/fb040dd7a280424b90c838a2bbfa9868/Certificate.pdf',
-      description: "CS169.2x Software as a Service",
     },
     {
       name: 'Code School',
-      description: "Ruby Bits",
+      content: "Ruby Bits",
       logo: '/images/courses/ruby-bits1.png',
       url: 'http://www.codeschool.com/users/demenchuk',
     }
