501956e2cb135d846c735d28d66508436c31107d
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-06-23 00:30:09 +0100

    Add uncompleted courses.

--- lib/courses.rb
+++ lib/courses.rb
@@ -2,27 +2,39 @@ module Demenchuk
   COURSES = [
     {
       name: 'BerkeleyX',
-      content: "CS169.1x Software as a Service",
-      logo: 'https://portland2013.drupal.org/sites/default/files/DCPDX-badge-round-I-go.png',
-      url: 'https://s3.amazonaws.com/verify.edx.org/downloads/62cf2d4f510f48ee95e44f01e6bc7243/Certificate.pdf',
-    },
-    {
-      name: 'BerkeleyX',
-      content: "CS169.1x Software as a Service",
+      content: 'CS169.1x Software as a Service',
       logo: '/images/courses/cs169.1x.png',
       url: 'https://s3.amazonaws.com/verify.edx.org/downloads/62cf2d4f510f48ee95e44f01e6bc7243/Certificate.pdf',
     },
     {
       name: 'BerkeleyX',
-      content: "CS169.2x Software as a Service",
+      content: 'CS169.2x Software as a Service',
       logo: '/images/courses/cs169.2x.png',
       url: 'https://s3.amazonaws.com/verify.edx.org/downloads/fb040dd7a280424b90c838a2bbfa9868/Certificate.pdf',
     },
     {
       name: 'Code School',
-      content: "Ruby Bits",
+      content: 'Ruby Bits',
       logo: '/images/courses/ruby-bits1.png',
       url: 'http://www.codeschool.com/users/demenchuk',
-    }
+    },
+    # {
+    #   name: 'Georgia Institute of Technology',
+    #   content: 'Software Defined Networking',
+    #   logo: '/images/courses/georgia-tech.png',
+    #   url: 'https://www.coursera.org/course/sdn',
+    # },
+    # {
+    #   name: 'Vanderbilt University',
+    #   content: 'Pattern-Oriented Software Architectures for Concurrent and Networked Software',
+    #   logo: '/images/courses/vanderbilt.png',
+    #   url: 'https://www.coursera.org/course/posa',
+    # },
+    # {
+    #   name: 'Stanford',
+    #   content: 'Introduction to Databases',
+    #   logo: '/images/courses/stanford.png',
+    #   url: 'https://www.coursera.org/course/db',
+    # }
   ]
 end
