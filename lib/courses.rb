db7663e4e839dbf21e64d628edd5b69e22aa68da
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-06-22 23:10:01 +0100

    Move courses to module

---
+++ lib/courses.rb
@@ -0,0 +1,22 @@
+module Demenchuk
+  COURSES = [
+    {
+      name: 'BerkeleyX',
+      description: "CS169.1x Software as a Service",
+      logo: '/images/courses/cs169.1x.png',
+      url: 'https://s3.amazonaws.com/verify.edx.org/downloads/62cf2d4f510f48ee95e44f01e6bc7243/Certificate.pdf',
+    },
+    {
+      name: 'BerkeleyX',
+      logo: '/images/courses/cs169.2x.png',
+      url: 'https://s3.amazonaws.com/verify.edx.org/downloads/fb040dd7a280424b90c838a2bbfa9868/Certificate.pdf',
+      description: "CS169.2x Software as a Service",
+    },
+    {
+      name: 'Code School',
+      description: "Ruby Bits",
+      logo: '/images/courses/ruby-bits1.png',
+      url: 'http://www.codeschool.com/users/demenchuk',
+    }
+  ]
+end
