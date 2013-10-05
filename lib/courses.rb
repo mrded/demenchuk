1a4ed107d4cb1a93daa889130265b9a1990d689c
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-10-05 23:24:12 +0100

    Update courses/confs style.

--- lib/courses.rb
+++ lib/courses.rb
@@ -1,40 +1,25 @@
 module Demenchuk
   COURSES = [
     {
-      name: 'DrupalCon',
-      content: 'Prague 2013',
-      logo: '/images/conferences/drupalcon_prague2013.png',
-      url: 'https://prague2013.drupal.org',
-    },
-    {
       name: 'BerkeleyX',
-      content: 'CS169.1x Software as a Service',
+      course: 'CS169.1x Software as a Service',
+      description: 'CS169.1x teaches the fundamentals for engineering long-lived software using Agile techniques to develop Software as a Service (SaaS) using Ruby on Rails.',
       logo: '/images/courses/cs169.1x.png',
       url: 'https://s3.amazonaws.com/verify.edx.org/downloads/62cf2d4f510f48ee95e44f01e6bc7243/Certificate.pdf',
     },
     {
       name: 'BerkeleyX',
-      content: 'CS169.2x Software as a Service',
+      course: 'CS169.2x Software as a Service',
+      description: 'CS169.2x teaches sophisticated SaaS+Agile skills, such as working with legacy code, building on basics from CS169.1x.',
       logo: '/images/courses/cs169.2x.png',
       url: 'https://s3.amazonaws.com/verify.edx.org/downloads/fb040dd7a280424b90c838a2bbfa9868/Certificate.pdf',
     },
-    {
-      name: 'Acquia',
-      content: 'Drupal 7 Search Introduction',
-      logo: '/images/courses/acquia-search_course.png',
-      url: "http://searchtng-acquia.talentlms.com",
-    },
-    {
-      name: 'Code School',
-      content: 'Ruby Bits',
-      logo: '/images/courses/ruby-bits1.png',
-      url: 'http://www.codeschool.com/users/demenchuk',
-    },
     # {
-    #   name: 'Georgia Institute of Technology',
-    #   content: 'Software Defined Networking',
-    #   logo: '/images/courses/georgia-tech.png',
-    #   url: 'https://www.coursera.org/course/sdn',
+    #   name: 'Code School',
+    #   course: 'Ruby Bits',
+    #   description: 'Learn the core bits every Ruby programmer should know. This Ruby tutorial builds upon the basics to keep enhancing your Ruby knowledge.',
+    #   logo: '/images/courses/ruby-bits1.png',
+    #   url: 'http://www.codeschool.com/users/demenchuk',
     # },
     # {
     #   name: 'Vanderbilt University',
