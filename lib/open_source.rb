cb9a3216fb360f7602153aa4d751518f32d216ce
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-11-24 23:07:07 +0000

    Add modules.

--- lib/open_source.rb
+++ lib/open_source.rb
@@ -7,19 +7,16 @@ module Demenchuk
         name: 'MNPP',
         type: 'Mac + Nginx + Percona + PHP',
         url: 'http://getmnpp.org',
-        overview: 'A high performance web server in a one-click installer.',
       },
       {
         name: 'Brew-emp',
         type: '(E)Nginx + MySQL + PHP Installer',
         url: 'https://github.com/mrded/brew-emp',
-        overview: 'Native way to setup web environment for Mac.',
       },
       {
         name: 'Reveally',
         type: 'Ruby on Rails app',
         url: 'https://github.com/elDante/reveally',
-        overview: 'Alliance website for Eve Online',
       }
     ],
     drupal: [
@@ -27,49 +24,51 @@ module Demenchuk
         name: 'HelloBar',
         type: 'Drupal module',
         url: 'http://drupal.org/project/hellobar',
-        overview: 'A Hello Bar is a simple notification bar to deliver a message and drive traffic to a specific call to action.',
       },
       {
         name: 'Blurry',
         type: 'Drupal module',
         url: 'http://drupal.org/project/blurry',
-        overview: 'This module can make your text fields blurry.',
       },
       {
         name: 'Panels Import',
         type: 'Drupal module',
         url: 'http://drupal.org/project/panels_import',
-        overview: 'This module provides an easy way to import previously exported Panels definitions from corresponding subfolders.',
       },
       {
         name: 'Panels Bootstrap Layouts',
         type: 'Drupal module',
         url: 'http://drupal.org/project/panels_bootstrap_layouts',
-        overview: "Panels' layouts using Twitter Bootstrap layout system as base.",
       },
       {
         name: 'Views Import',
         type: 'Drupal module',
         url: 'http://drupal.org/project/views_import',
-        overview: 'This module provides an easy way to import previously exported Views definitions.',
       },
       {
         name: 'Views Bootstrap',
         type: 'Drupal module',
         url: 'http://drupal.org/project/views_bootstrap',
-        overview: "This module provides Twitter Bootstrap's Components integration with Views module.",
       },
       {
         name: 'Views Stapel',
         type: 'Drupal module',
         url: 'http://drupal.org/project/views_stapel',
-        overview: 'An experimental jQuery plugin that will group thumbnails by a shared data-attribute into a pile. When clicking on the pile, the thumbnails that belong to that pile will be spread into a grid using CSS transitions.',
       },
       {
         name: 'Views Job Board',
         type: 'Drupal module',
         url: 'http://drupal.org/project/views_job_board',
-        overview: "This module can render table with job posts and group them by company, that's it.",
+      },
+      {
+        name: 'Email Auto Login',
+        type: 'Drupal module',
+        url: 'http://drupal.org/project/email_auto_login',
+      },
+      {
+        name: 'User Relationship Locator',
+        type: 'Drupal module',
+        url: 'http://drupal.org/project/user_relationship_locator',
       }
     ]
   }
