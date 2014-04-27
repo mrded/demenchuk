e73f3de7e4db4812f0aa6e600899c40cba3a9cf6
Dmitry Demenchuk <dmitry@demenchuk.me>
2014-04-27 21:12:46 +0100

    Add DrupalCamp London 2014 conference

--- lib/conferences.rb
+++ lib/conferences.rb
@@ -7,5 +7,12 @@ module Demenchuk
       logo: '/images/conferences/drupalcon_prague2013.png',
       url: 'https://prague2013.drupal.org',
     },
+    {
+      name: 'DrupalCamp London 2014',
+      course: '',
+      description: "A camp is a one to two day event that focuses on many aspects of Drupal in one location. Its focus is knowledge sharing amongst the community. It has been the 2nd largest Drupalcamp in Europe ever!",
+      logo: '/images/conferences/drupalcamp_london2014.png',
+      url: 'http://2014.drupalcamplondon.co.uk',
+    },
   ]
 end
