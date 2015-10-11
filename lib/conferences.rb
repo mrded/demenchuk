85d49da9e8e8775e361e2f2cc38a8e763225fcfa
Dmitry Demenchuk <dmitry@demenchuk.me>
2015-10-11 22:25:09 +0100

    Add new drupalcon conference

--- lib/conferences.rb
+++ lib/conferences.rb
@@ -2,21 +2,23 @@ module Demenchuk
   CONFERENCES = [
     {
       name: 'DrupalCon Prague 2013',
-      description: "DrupalCon is the biannual gathering of the world's top Drupal contributors and businesses, which makes it a great place for introductions - not just to the people present but also to Drupal itself.",
       logo: '/images/conferences/drupalcon_prague2013.png',
       url: 'https://prague2013.drupal.org',
     },
     {
       name: 'DrupalCamp London 2014',
-      description: "A camp is a one to two day event that focuses on many aspects of Drupal in one location. Its focus is knowledge sharing amongst the community. It has been the 2nd largest Drupalcamp in Europe ever!",
       logo: '/images/conferences/drupalcamp_london2014.png',
       url: 'http://2014.drupalcamplondon.co.uk',
     },
     {
       name: 'DrupalCon Amsterdam 2014',
-      description: "DrupalCon is the heartbeat of the Drupal community. It’s an expression of our software innovation, our commitment to collaboration, and our extreme nerdiness.",
       logo: '/images/conferences/drupalcon_amsterdam2014.png',
       url: 'https://amsterdam2014.drupal.org',
+    },
+    {
+      name: 'DrupalCon Barcelona 2015',
+      logo: '/images/conferences/drupalcon_barcelona2015.png',
+      url: 'https://events.drupal.org/barcelona2015',
     },
   ]
 end
