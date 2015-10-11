7310e397c8ff439605706f8d9220eb34a1288b95
Dmitry Demenchuk <dmitry@demenchuk.me>
2015-10-11 22:44:26 +0100

    Split up name and year.

--- lib/conferences.rb
+++ lib/conferences.rb
@@ -1,22 +1,26 @@
 module Demenchuk
   CONFERENCES = [
     {
-      name: 'DrupalCon Prague 2013',
+      name: 'DrupalCon Prague',
+      year: '2013'
       logo: '/images/conferences/drupalcon_prague2013.png',
       url: 'https://prague2013.drupal.org',
     },
     {
-      name: 'DrupalCamp London 2014',
+      name: 'DrupalCamp London',
+      year: '2014',
       logo: '/images/conferences/drupalcamp_london2014.png',
       url: 'http://2014.drupalcamplondon.co.uk',
     },
     {
-      name: 'DrupalCon Amsterdam 2014',
+      name: 'DrupalCon Amsterdam',
+      year: '2014',
       logo: '/images/conferences/drupalcon_amsterdam2014.png',
       url: 'https://amsterdam2014.drupal.org',
     },
     {
-      name: 'DrupalCon Barcelona 2015',
+      name: 'DrupalCon Barcelona',
+      year: '2015',
       logo: '/images/conferences/drupalcon_barcelona2015.png',
       url: 'https://events.drupal.org/barcelona2015',
     },
